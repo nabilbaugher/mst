1814a152b20940edee68ad075a5f6c04bb7fc19e
Nabil <nabilbaugher@gmail.com>
2022-12-03 20:15:35 -0500

    add file to generate our map types

---
+++ map_generator.py
@@ -0,0 +1,12 @@
+from mst_prototype import map_builder
+
+def generate_trick_maps():
+    """
+    Generates a set of trick maps that all follow the same path to the goal
+    but have different extraneous paths.
+    
+    Returns:
+        A list of maps where each map is represented as the arguments to
+        map_builder() which are nrows, ncols, black, path, start.
+    """
+    pass
