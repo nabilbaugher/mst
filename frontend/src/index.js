490de9803cc178d392c78dcfa00a6309e9a3e3f5
zsheill <zsheill7@gmail.com>
2022-12-04 19:32:35 -0500

    cleaned up a little

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -44,15 +44,8 @@ class GridSystem {
 			}
 		}
 
-		// this.insertEntry({ 
-		// 	tester_id: uuid(), 
-		// 	created_at: String(Date.now()), 
-		// 	keystroke_sequences: {
-		// 		1: ["left", "right", "up", "down", "left", "win"],
-		// 		2: ["right", "up", "down"],
-		// 		3: ["left", "down", "right", "win"]
-		// }})
-		
+		this.original_playerX = playerX;
+		this.original_playerY = playerY;
 
 		// Random supabase test
 
@@ -70,6 +63,8 @@ class GridSystem {
 
 		document.addEventListener("keydown", this.#movePlayer);
 	}
+
+	// We need a function to reset 
 
 	async insertEntry({tester_id, created_at, keystroke_sequences}) {
 		try{
@@ -185,15 +180,6 @@ class GridSystem {
 		this.canvas.style.marginLeft = center.x
 		this.canvas.style.marginTop = center.y;
 
-
-
-		// // Create button too
-		// let btn = document.createElement("button");
-		// btn.innerHTML = "Next trial"
-		// // btn.style.position = "absolute";
-		// btn.style.top = this.canvas.offsetHeight + this.canvas.height;
-		// document.body.appendChild(btn)
-
 		document.body.appendChild(this.canvas);
 
 		return this.context;
@@ -235,8 +221,23 @@ class GridSystem {
 		let btn = document.createElement("button");
 		btn.innerHTML = "Next trial"
 		btn.style.position = "absolute";
-		btn.style.left = "50%";
+		btn.style.left = center.x;
 		btn.style.top = "75%";
+		btn.onclick = function () {
+			console.log("Button is clicked");
+			
+			if (this.player_won == false) {
+				this.insertEntry({ 
+					tester_id: this.current_user_id, 
+					created_at: String(Date.now()), 
+					keystroke_sequences: {
+						1: this.current_keystroke_sequence
+				}})
+			}
+			
+
+			// [TODO] Reset trial
+		};
 		document.body.appendChild(btn)
 
 		
@@ -246,6 +247,8 @@ class GridSystem {
 
 		if (this.player_won == true) {
 			// Upload keystroke data
+			this.current_keystroke_sequence.push("won")
+
 			this.insertEntry({ 
 				tester_id: this.current_user_id, 
 				created_at: String(Date.now()), 
@@ -275,104 +278,3 @@ const gridMatrix = [
 
 const gridSystem = new GridSystem(gridMatrix, 1, 1);
 gridSystem.render();
-
-
-
-
-// class GridSystem {
-// 	constructor(matrix) {
-// 		this.matrix = matrix;
-// 		this.uiContext = this.#getContext(420, 580, "#000");
-// 		this.outlineContext = this.#getContext(0, 0, "#444");
-// 		this.topContext = this.#getContext(0, 0, "#111", true);
-// 		this.cellSize = 40;
-// 		this.padding = 2;
-// 	}
-
-// 	#getCenter(w, h) {
-// 		return {
-// 			x: window.innerWidth / 2 - w / 2 + "px",
-// 			y: window.innerHeight / 2 - h / 2 + "px"
-// 		};
-// 	}
-
-// 	#getContext(w, h, color = "#111", isTransparent = false) {
-// 		this.canvas = document.createElement("canvas");
-// 		this.context = this.canvas.getContext("2d");
-// 		this.width = this.canvas.width = w;
-// 		this.height = this.canvas.height = h;
-// 		this.canvas.style.position = "absolute";
-// 		this.canvas.style.background = color;
-// 		if (isTransparent) {
-// 			this.canvas.style.backgroundColor = "transparent";
-// 		}
-// 		const center = this.#getCenter(w, h);
-// 		this.canvas.style.marginLeft = center.x
-// 		this.canvas.style.marginTop = center.y;
-// 		document.body.appendChild(this.canvas);
-
-// 		return this.context;
-// 	}
-
-// 	render() {
-// 		const w = (this.cellSize + this.padding) * this.matrix[0].length - (this.padding);
-// 		const h = (this.cellSize + this.padding) * this.matrix.length - (this.padding);
-
-// 		this.outlineContext.canvas.width = w;
-// 		this.outlineContext.canvas.height = h;
-
-// 		const center = this.#getCenter(w, h);
-// 		this.outlineContext.canvas.style.marginLeft = center.x
-// 		this.outlineContext.canvas.style.marginTop = center.y;
-
-// 		this.topContext.canvas.style.marginLeft = center.x
-// 		this.topContext.canvas.style.marginTop = center.y;
-
-// 		for (let row = 0; row < this.matrix.length; row ++) {
-// 			for (let col = 0; col < this.matrix[row].length; col ++) {
-// 				const curr_val = this.matrix[row][col];
-
-// 				// 8 is our player character, player color is moccasin
-// 				const color_map = {0: '#9c9c9c', 1: 'white', 2: '#d074a4', 3: '#b0943d', 4: 'white', 5: '#a1c38c', 6: 'white', 7: '#f5f5dc', 8: 'moccasin'};
-
-// 				const color_assigned = color_map[curr_val];
-					
-// 				this.outlineContext.fillStyle = color_assigned;
-// 				this.outlineContext.fillRect(col * (this.cellSize + this.padding),
-// 				row * (this.cellSize + this.padding),
-// 				this.cellSize, this.cellSize);
-// 			}
-// 		}
-
-// 		this.uiContext.font = "20px Courier";
-// 		this.uiContext.fillStyle = "white";
-// 		this.uiContext.fillText("Grid Based System", 20, 30);
-// 	}
-// }
-
-// const gridMatrix = [
-//     [3, 3, 3, 3, 3, 3, 3, 3, 3],
-//     [3, 3, 3, 3, 0, 3, 0, 3, 3],
-//     [3, 3, 3, 3, 0, 3, 0, 3, 3],
-//     [3, 5, 6, 6, 6, 6, 6, 6, 3],
-//     [3, 6, 3, 3, 3, 3, 3, 6, 3],
-//     [3, 6, 6, 6, 6, 6, 6, 6, 3],
-//     [3, 3, 0, 0, 3, 3, 3, 3, 3],
-//     [3, 3, 3, 3, 3, 3, 3, 3, 3]
-// ];
-
-// // const gridMatrix = [
-// // 	[1, 1, 1, 1, 1, 1, 1],
-// // 	[1, 0, 0, 0, 0, 0, 1],
-// // 	[1, 0, 0, 0, 0, 0, 1],
-// // 	[1, 0, 0, 0, 0, 0, 1],
-// // 	[1, 0, 1, 1, 1, 0, 1],
-// // 	[1, 0, 0, 0, 1, 0, 1],
-// // 	[1, 0, 1, 1, 1, 0, 1],
-// // 	[1, 0, 0, 0, 0, 0, 1],
-// // 	[1, 1, 1, 1, 1, 1, 1]
-// // ];
-
-
-// const gridSystem = new GridSystem(gridMatrix);
-// gridSystem.render();
