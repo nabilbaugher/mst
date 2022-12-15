e4d6c3b35648f4dabcba9177b8e9a4e8ed6adcd7
zsheill <zsheill7@gmail.com>
2022-12-14 20:47:23 -0500

    added data, started data analysis

---
+++ backend/random_data_analysis.py
@@ -0,0 +1,9 @@
+# Random data analysis of our trials_rows.csv
+# Using data_parser.py
+
+
+# Things that we want to learn:
+
+# Average total number of steps for each maze for humans
+# Average total number of steps for each maze for models
+
