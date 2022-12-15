5500b1669fb4466417cf0905df28bfd74e625bf1
zsheill <zsheill7@gmail.com>
2022-12-14 21:51:41 -0500

    data analysis notes

--- backend/random_data_analysis.py
+++ backend/random_data_analysis.py
@@ -7,8 +7,26 @@
 # Average total number of steps for each maze for humans
 # Average total number of steps for each maze for models
 
+# Average human time, standard deviation
+
+# Mazes changing over time
+#   Find some way to represent the fact that the first 15 mazes have same exit locations
+#   Second one has different exit locations
+#   Graph, for one of our best models, human performance on each maze, maybe up then down
+
+# How correlated is memoryless performance vs memory performance
+
+# Previous models
+#   Make the step cells equations
+#   Make sure the other previous equations work on our mazes
+
+# Run the previous models on the new mazes:
+# * Expected utility
+# * Discounted utility
+# * Probability weighted utility
+# * Combined
+# * Heuristics
+
 # Cool things to do:
 # Track backtracking - did they do "left" then "right"
 # See how similar human traversals are to each other
-
-# 
